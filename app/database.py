d3df7650135c619a335dcb882cacf148b700748b
Abhishek Yadav <abhishek26321@gmail.com>
2025-11-05 12:33:31 +0530

    Added docker compose

--- app/database.py
+++ app/database.py
@@ -2,12 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./alerts.db"
 
-DATABASE_URL = "postgresql://username:password@localhost/alertdb"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://alert_user:alert_password@localhost:5433/alertdb"
 
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False } if DATABASE_URL.startswith("sqlite") else {}
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False } if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
